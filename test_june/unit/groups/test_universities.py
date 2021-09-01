adbd3462bee5d569ff69bf8b87301963a1986b10
mike-the-bug <MichaelKaefer@gmx.net>
2021-09-01 18:04:53 +0200

    Changed input to fit german data the value of SuperArea.name can be anything or simply left empty as it will be always named after the soa during simulation due to changes of university.py Area.name has to be considered here and be named after the oa otherwise it will try to compare a given name with oa from the csv hence it will never find an uni which results in an index out of range error

--- test_june/unit/groups/test_universities.py
+++ test_june/unit/groups/test_universities.py
@@ -4,18 +4,17 @@ from june.geography import Area, Areas, SuperArea, Geography, SuperAreas
 
 
 def test__university_init():
-    university = University(coordinates = np.array([1, 2]), n_students_max = 500)
-    assert (university.coordinates == np.array([1,2])).all()
+    university = University(coordinates=np.array([1, 2]), n_students_max=500)
+    assert (university.coordinates == np.array([1, 2])).all()
     assert university.n_students_max == 500
 
+
 def test__university_for_super_areas():
-    super_area = SuperArea(name="mainz", areas=None, coordinates=[49.974186337513885, 8.241497125821837])
-    area = Area(name='mainz_central', super_area=super_area, coordinates=super_area.coordinates)
+    super_area = SuperArea(name='Mainz', areas=None, coordinates=[49.974186337513885, 8.241497125821837])
+    area = Area(name='D073150000000', super_area=super_area, coordinates=super_area.coordinates)
     areas = Areas([area])
     super_area.areas = areas
     super_areas = SuperAreas([super_area])
-    unis  = Universities.for_areas(areas)
+    unis = Universities.for_areas(areas)
     durham_uni = unis[0]
     assert durham_uni.n_students_max == 31064
-
-
