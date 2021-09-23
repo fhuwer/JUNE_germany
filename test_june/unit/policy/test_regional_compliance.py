16049a1a34712d7744e329cf9c2dfe4df47a5cd2
mike-the-bug <MichaelKaefer@gmx.net>
2021-09-23 14:04:45 +0200

    Changed region name to fit germany

--- test_june/unit/policy/test_regional_compliance.py
+++ test_june/unit/policy/test_regional_compliance.py
@@ -7,10 +7,10 @@ class TestSetRegionCompliance:
         regional_compliance = RegionalCompliance(
             start_time="2020-05-01",
             end_time="2020-09-01",
-            compliances_per_region={"London": 1.5},
+            compliances_per_region={"Mainz": 1.5},
         )
         regional_compliances = RegionalCompliances([regional_compliance])
-        region = Region(name="London")
+        region = Region(name="Mainz")
         regions = Regions([region])
         regional_compliances.apply(regions=regions, date = "2020-05-05")
         assert region.regional_compliance == 1.5
@@ -28,10 +28,10 @@ class TestSetTiers:
         tiered_lockdown = TieredLockdown(
             start_time="2020-05-01",
             end_time="2020-09-01",
-            tiers_per_region={"London": 2.},
+            tiers_per_region={"Mainz": 2.},
         )
         tiered_lockdowns = TieredLockdowns([tiered_lockdown])
-        region = Region(name="London")
+        region = Region(name="Mainz")
         regions = Regions([region])
         tiered_lockdowns.apply(regions=regions, date = "2020-05-05")
         assert region.policy["lockdown_tier"] == 2
